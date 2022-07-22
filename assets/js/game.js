79ea4da7c22b9c9567272e97e07089f2e657e02e
p5eudoDjnn <alexander.havers@gmail.com>
2022-07-22 16:18:38 -0400

    variables and concatenation

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("ALERT INIT 4 GAME!");
+// creating a function named "fight"
+
+function fight() {
+
+  let playerName = window.prompt("What is your robot's Name?");
+  console.log(playerName);
+  console.log("This is a string, good for messages");
+  // math
+  console.log(10 + 10);
+  // what is this?!
+  console.log("Our robot's name is " + playerName);
+  // Concatenating
+  console.log("Your robot, " + playerName + ",has won!");
+
+  window.alert(playerName);
+};
+
+fight();
